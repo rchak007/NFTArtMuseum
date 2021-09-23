913e716dc670aea2b5b7e4667edf6632409847a5
Chakravarti Raghavan <chakravartiraghavan@Chakravartis-MacBook-Pro.local>
2021-09-23 04:29:02 -0700

    streamlit

--- NFTArtMuseumSL.py
+++ NFTArtMuseumSL.py
@@ -9,6 +9,8 @@ from getpass import getpass
 import pandas as pd
 import getpass
 import streamlit as st
+import SessionState
+import webbrowser
 
 from etherscan import Etherscan
 import json
@@ -31,10 +33,14 @@ def admin():
     w1.middleware_onion.inject(geth_poa_middleware, layer=0)
     account_contract_owner = Account.from_key(museum_private_key)
     from web3.auto.infura import w3
+    
     return WEB3_INFURA_API_KEY, WEB3_INFURA_PROJECT_ID, nft_museum_address, museum_private_key, eth, w1, account_contract_owner, w3
 
 zero_address = '0x0000000000000000000000000000000000000000'
 WEB3_INFURA_API_KEY, WEB3_INFURA_PROJECT_ID, nft_museum_address, museum_private_key, eth, w1, account_contract_owner, w3 = admin()
+
+
+
 
 ## read NFTArtMuseum contract ABI
 with open("NFTArtMuseumABI.txt") as f:
@@ -263,26 +269,86 @@ def approve_buy_nft(account_artist, artist_private_key, account_buyer, buyer_pri
 
 
 
+# def test1():
+#     place_holder = st.empty()
+#     nft_name_test = place_holder.text_input("Please enter your NFT name", key=  "nft_name_test")
+#     return place_holder, nft_name_test
+
 def main_routine():
     # set up admin stuff.
     #WEB3_INFURA_API_KEY, WEB3_INFURA_PROJECT_ID, museum_private_key, eth, w1, account_contract_owner, w3 = admin()
     success = 1
     i=1
     museum_action = 0
-    
-    page = st.sidebar.selectbox("Choose ", ["Register Art", "Buy NFT"])
+    url = 'https://kovan.etherscan.io/address/0x52e95cf058c086d845b6938ccd1120c2fcf19a58'
+    link = '[NFT Art Museum Conrtact](https://kovan.etherscan.io/address/0x52e95cf058c086d845b6938ccd1120c2fcf19a58)'
+    page = st.sidebar.selectbox("Choose ", ["Register Art", "Buy NFT", "Test"])
     st.title('Welcome to NFT Art Museum')
+    if page == "Test":
+        st.write("a logo and text next to eachother")
+        col1, mid, col2 = st.columns([100,100,520])
+        with col1:
+            st.image('NFT2AI.png', width=300)
+        with col2:
+            st.write('A Name')
+        #st.session_state['key'] = 'value'
+        #st.write(st.session_state.key)
+#         place_holder, nft_name_test = test1()
+#         if st.button('Clear'):
+#             place_holder.empty()
+#             place_holder, nft_name_test = test1()
+        session = SessionState.get(run_id=0)
+        slider_element = st.empty()
+        if st.button("Reset"):
+            session.run_id += 1
+        key1 = 'slider' + str(session.run_id)
+        key2 = 'art' + str(session.run_id)
+        #slider_element.slider("Slide me!", 0, 100, key=key1)
+        #art = st.text_input("Please enter Artist name", key=  key2)
+        #st.write(pd.DataFrame(df))
+        #print(df.head())
+        #df1 = df.loc[df['Key']=='xx']
+        #print(df1.head())
+        #st.write(df1['Address'])
     if page == "Register Art":
-        st.header("NFT Art Registry page.")
+        session = SessionState.get(run_id=0)
+        #session1 = SessionState.get(run_id=0)
+        st.header("NFT Art Registry")
+        col1, mid, col2 = st.columns([1,10,30])
+        with col1:
+            st.image('NFT2AI.png',  width=250)
+        with col2:
+            st.image('NFT1.png',  width=250)
+        placeholder = st.empty()
+        placeholder2 = st.empty()
+        placeholder3 = st.empty()
+        placeholder4 = st.empty()
+        placeholder5 = st.empty()
+        placeholder6 = st.empty()
         #st.write("Please select a page on the left.")
-        nft_name = st.text_input("Please enter your NFT name", key=  "nft_name")
-        artist_name = st.text_input("Please enter Artist name", key=  "artist_name")
-        token_uri = st.text_input("Please enter your Token URI in IPFS format ", key=  "token_uri")
-        price = st.slider("Select the price of NFT in Wei", 10000, 50000000)
-        st.text('Selected: {}'.format(price))
-        artist_private_key = st.text_input("Please enter your private key: ", key=  "artist_private_key", type="password")
+        #nft_name = st.text_input("Please enter your NFT name", key=  "nft_name")
+        
+        #artist_name = st.text_input("Please enter Artist name", key=  "artist_name")
+        key1 = 'nft' + str(session.run_id)
+        key2 = 'art' + str(session.run_id)
+        key3 = 'uri' + str(session.run_id)
+        key4 = 'price' + str(session.run_id)
+        key5 = 'priv_key' + str(session.run_id)
+        key6 = 'price_display' + str(session.run_id)
+        
+        nft_name = placeholder.text_input("Please enter your NFT name", key=key1)
+        artist_name = placeholder2.text_input("Please enter Artist name", key=key2)
+        token_uri = placeholder3.text_input("Please enter your Token URI in IPFS format ", key=  key3)
+        artist_private_key = placeholder5.text_input("Please enter your private key: ", key=  key5, type="password")
+        price = placeholder4.slider("Select the price of NFT in Wei", 10000, 50000000, key=key4)
+        placeholder6.text('Selected: {}'.format(price))        #artist_private_key = placeholder.text_input("Please enter your private key: ", key=  "artist_private_key", type="password")
         #st.button('Register Art')
         museum_action = 0
+        if st.button('Clear'):
+            session.run_id += 1
+            #session1.run_id += 1
+
+            #artist_private_key = placeholder.text_input("Please enter your private key: ", value='', key=  "artist_private_key")
         if st.button('Register Art'):
             try:
                 account_artist = Account.from_key(artist_private_key)
@@ -292,6 +358,7 @@ def main_routine():
                 error = 1
                 return error, error_message
             #museum_action = 1
+        
             chain_id, gasEstimate, gas_price, nonce = prep_transaction(account_contract_owner.address)
             register_nft_txn, register_signed_txn_hash, register_signed_txn_rawTransaction, register_signed_txn_r, register_signed_txn_s, register_signed_txn_v, register_sent_txn, error_sign, error_signing_txn, error_send, error_sending_txn = execute_art_registry(museum_private_key, account_artist.address, nft_name, artist_name, price, token_uri, chain_id, gasEstimate, gas_price, nonce)
             if error_sign == 1:
@@ -301,7 +368,7 @@ def main_routine():
                 print('NFT Art Register - Error signing transaction: ', error_sending_txn)
                 return error_sending_txn
             if error_sign != 1 and error_send != 1:
-                st.write('Art registry successful!! congrats. Your txn = ', register_signed_txn_hash)
+                st.write('Art registry successful!! congrats!')
                 print('register_nft_txn = ', register_nft_txn)
                 print('register_signed_txn_hash = ', register_signed_txn_hash)
                 print('register_signed_txn_rawTransaction = ', register_signed_txn_rawTransaction)
@@ -311,14 +378,135 @@ def main_routine():
                 print('register_sent_txn = ', register_sent_txn)
                 #register_get_txn = w1.eth.getTransaction(register_sent_txn)
                 #print('register_get_txn = ', register_get_txn)
-                artist_private_key = ''
-                nft_name = ''
-                artist_name = ''
-                token_uri = ''
+                # Check if this Artist is new then store the key
+                df = pd.read_csv('NFTOwners.csv')
+                df1 = df.loc[df['Key']==artist_private_key]
+                if df1.empty:
+                    df = df.append({'Address': account_artist.address, 'Key': artist_private_key }, ignore_index=True)
+                    df.to_csv('NFTOwners.csv', index=False)
+                #print(df1.head())
+                #st.write(df1['Address'])
+                st.write('Click on link NFT Art Museum button to go to contract for details')
+                st.markdown(link, unsafe_allow_html=True)
+                #if st.button('NFT Art Museum'):
+                    #webbrowser.open_new_tab(url)
                 return success, "Succcess"
         #st.write(df)
-    elif page == "Exploration":
-        st.title("Data Exploration")
+    elif page == "Buy NFT":
+        st.header("NFT Buy Art page.")
+        #st.write("Please select a page on the left.")
+        buy_token_id = st.number_input('Input Token id you want to buy: ', format="%f")
+        buy_token_id = int(buy_token_id)
+        buyer_private_key = st.text_input("Please enter your buyer private key : ", key=  "buyer_private_key", type="password")
+        if buy_token_id != 0:
+            l_art_collection = contract_NFTArtMuseuminstance.functions.art_collection(buy_token_id).call()
+            st.write('NFT Name: ', l_art_collection[0])
+            st.write('NFT Artist: ', l_art_collection[1])
+            st.write('NFT Price: ', l_art_collection[2])
+            l_curr_owner = contract_NFTArtMuseuminstance.functions.ownerOf(buy_token_id).call()
+            st.write('Current NFT Owner: ', l_curr_owner)
+        #nft_name = st.text_input("Please enter your NFT name", key=  "nft_name")
+        #artist_name = st.text_input("Please enter Artist name", key=  "artist_name")
+        #token_uri = st.text_input("Please enter your Token URI in IPFS format ", key=  "token_uri")
+        #price = st.number_input('Enter Price of NFT: ', format="%f")
+            price = int(l_art_collection[2])
+            df = pd.read_csv('NFTOwners.csv')
+            df1 = df.loc[df['Address']==l_curr_owner]
+            if not df1.empty:
+                artist_private_key_buy = df1.iloc[0]['Key']
+        #st.write('artist private key', artist_private_key_buy)
+        #st.write('type artist private key', type(artist_private_key_buy))
+        #else:
+            #artist_private_key_buy = st.text_input("Please enter Artist private key : ", key=  "artist_private_key_buy", type="password")
+        #st.text('Selected: {}'.format(price))
+        if st.button('Buy Art'):
+            # Buyer info
+            try:
+                account_buyer = Account.from_key(buyer_private_key)
+            except Exception as error_message:
+                print('ERROR: ',error_message)
+                st.write("Buyer Private key address error", error_message)
+                error = 1
+                return error, error_message
+            print('Buyer balance = ', eth.get_eth_balance(address=account_buyer.address))
+            st.write('Buyer Address = ', account_buyer.address)
+            buyer_bal = eth.get_eth_balance(address=account_buyer.address)
+            st.write('Buyer balance = ', buyer_bal )
+            # Artist info
+            try:
+                account_artist_buy = Account.from_key(artist_private_key_buy)
+            except Exception as error_message:
+                print('ERROR: ',error_message)
+                st.write("Artist Private key address error", error_message)
+                error = 1
+                return error, error_message
+            chain_id, gasEstimate, gas_price, nonce = prep_transaction(account_artist_buy.address)
+            print('****************** Approval *************')
+            print('Chain id = ', chain_id)
+            print('gasEstimate = ', gasEstimate)
+            print('gasPrice = ', gas_price)
+            print('Nonce = ', nonce)
+            # call Approval
+            approve_nft_txn, approve_signed_nft_txn_hash, approve_signed_nft_txn_rawTransaction, approve_signed_nft_txn_r, approve_signed_nft_txn_s, approve_signed_nft_txn_v, approve_sent_txn, error_sign, error_signing_txn, error_send, error_sending_txn = execute_approval(artist_private_key_buy, account_buyer.address, buy_token_id, chain_id, gasEstimate, gas_price, nonce)
+            if error_sign == 1:
+                print('Owner approval - Error signing transaction: ', error_signing_txn)
+                return error_sign, error_signing_txn
+            if error_send == 1:
+                print('Owner approval - Error signing transaction: ', error_sending_txn)
+                return error_send, error_sending_txn
+            if error_sign != 1 and error_send != 1:
+                print('Approval successful.')
+                print('approve_nft_txn = ', approve_nft_txn)
+                print('approve_signed_nft_txn_hash = ', approve_signed_nft_txn_hash)
+                print('approve_signed_nft_txn_rawTransaction = ', approve_signed_nft_txn_rawTransaction)
+                print('approve_signed_nft_txn_r = ', approve_signed_nft_txn_r)
+                print('approve_signed_nft_txn_s = ', approve_signed_nft_txn_s)
+                print('approve_signed_nft_txn_v = ', approve_signed_nft_txn_v)
+                print('approve_sent_txn = ', approve_sent_txn)
+                #approve_get_txn = w1.eth.getTransaction(approve_sent_txn)
+                #print('approve_get_txn = ', approve_get_txn)
+                # Approval is success now execute the Buy and Trasnfer function.
+                chain_id, gasEstimate, gas_price, nonce = prep_transaction(account_buyer.address)
+                print('****************** Buy/Transfer *************')
+                print('Chain id = ', chain_id)
+                print('gasEstimate = ', gasEstimate)
+                print('gasPrice = ', gas_price)
+                print('Nonce = ', nonce)
+                # call BuyNFT
+                buy_nft_txn, signed_buy_nft_txn_hash, signed_buy_nft_txn_rawTransaction, signed_buy_nft_txn_r, signed_buy_nft_txn_s, signed_buy_nft_txn_v, buy_nft_sent_txn, error_sign, error_signing_txn, error_send, error_sending_txn = execute_buyNFT(buyer_private_key, account_artist_buy.address, price, buy_token_id, chain_id, gasEstimate, gas_price, nonce)
+                if error_sign == 1:
+                    print('BuyNFT - Error signing transaction: ', error_signing_txn)
+                    return error_sign, error_signing_txn
+                if error_send == 1:
+                    print('BuyNFT - Error signing transaction: ', error_sending_txn)
+                    return error_send, error_sending_txn
+                if error_sign != 1 and error_send != 1:
+                    print('Buy-Trasnfer successful.')
+                    print('buy_nft_txn = ', buy_nft_txn)
+                    print('signed_buy_nft_txn_hash = ', signed_buy_nft_txn_hash)
+                    print('signed_buy_nft_txn_rawTransaction = ', signed_buy_nft_txn_rawTransaction)
+                    print('signed_buy_nft_txn_r = ', signed_buy_nft_txn_r)
+                    print('signed_buy_nft_txn_s = ', signed_buy_nft_txn_s)
+                    print('signed_buy_nft_txn_v = ', signed_buy_nft_txn_v)
+                    print('buy_nft_sent_txn = ', buy_nft_sent_txn)
+                    # now store the Buyer private key since they are the new Owner and can be bought later
+                    df = pd.read_csv('NFTOwners.csv')
+                    df1 = df.loc[df['Key']==buyer_private_key]
+                    if df1.empty:
+                        df = df.append({'Address': account_buyer.address, 'Key': buyer_private_key }, ignore_index=True)
+                        df.to_csv('NFTOwners.csv', index=False)
+                        #buy_nft_get_txn = w1.eth.getTransaction(buy_nft_sent_txn)
+                        #print('buy_nft_get_txn = ', buy_nft_get_txn)
+                    st.write('Success on Buying NFT. Congrats!! ')
+                    st.write('Click on link NFT Art Museum link to go to contract for details')
+                    st.markdown(link, unsafe_allow_html=True)
+                    #if st.button('NFT Art Museum'):
+                        #webbrowser.open_new_tab(url)
+                    return 1, 'Approve and Buy/Trasfer success'
+            #print('Buyer balance = ', eth.get_eth_balance(address=account_artist_buy.address))
+            #st.write('Buyer Address = ', account_buyer.address)
+            #buyer_bal = eth.get_eth_balance(address=account_buyer.address)
+            #st.write('Buyer balance = ', buyer_bal )            
         #x_axis = st.selectbox("Choose a variable for the x-axis", df.columns, index=3)
         #y_axis = st.selectbox("Choose a variable for the y-axis", df.columns, index=4)
         #visualize_data(df, x_axis, y_axis)
